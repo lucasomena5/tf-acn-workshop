0e439314c09befe240cad26674c3c8b00756b36b
Lucas Omena <lucasomena5@gmail.com>
2022-03-18 21:49:29 -0300

    ACN Terraform Workshop - first commit #20220318

--- hands-on/aws-ec2/scripts/startup/install.ps1
+++ hands-on/aws-ec2/scripts/startup/install.ps1
@@ -4,3 +4,9 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs
 
 #######################################################################################################
+
+Set-ExecutionPolicy Bypass
+
+Get-Package -Provider chocolatey -Force
+
+Install-Package adobereader, 7zip, firefox, notepadplusplus, teamviewer, vlc -Force
